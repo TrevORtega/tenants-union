62d6d5353f9057d5fd023fbd897fa19f1a97ff2b
trevortega <trevor.ortega.12@gmail.com>
2023-10-19 23:15:07 -0700

    Refactor to look nice don't mind me

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+// i am LAZY; i have not added them all as you can see
 const unions = [
     {id: "pstu", title: "Puget Sound Tenants Union", isATUN: true, x: 47.584715, y:-122.302908},
     {id: "vtu", title: "Vancouver Tenants Union", isATUN: false, x: 49.24966, y: -123.11934},
@@ -5,8 +6,8 @@ const unions = [
     {id: "pvtu", title: "Vally Tenants Union", isATUN: true, x: 33.448, y: -112.074},
     {id: "ttu", title: "Tucson Tenants Union", isATUN: false, x: 32.2534, y: -110.9117},
 ];
-
-const map = L.map('map').setView([39.836546, -102.051843], 3);
+const centerOfAmericaKindaSorta = [39.836546, -102.051843];
+const map = L.map('map').setView(centerOfAmericaKindaSorta, 3);
 
 const tiles = L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
     maxZoom: 19,
@@ -16,7 +17,7 @@ const tiles = L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
 const addMarker = (id, title, isATUN, x, y) => {
     const size = isATUN ? [30, 30] : [20, 30]; 
     const icon = L.icon({
-        iconUrl: isATUN ? './redIcon.png' : 'whiteIcon.png',
+        iconUrl: isATUN ? './assets/redIcon.png' : './assets/whiteIcon.png',
 
         
         iconSize: size,
@@ -44,6 +45,8 @@ const addMarker = (id, title, isATUN, x, y) => {
 const unionContainer = document.getElementById("card-container");
 unions.forEach(u => {
     addMarker(u.id, u.title, u.isATUN, u.x, u.y);
+    // This creates a little card with info for each tenant union
+    // I haven't figured out a design for it yet so don't worry yet
     const child = document.createElement("div");
     child.setAttribute("id", u.id)
     child.setAttribute("class", "union")
